b86b2d300678d67e43e8104da997ccc365540d08
hossein sharifi <hosseinsharifi@hotmail.com>
2018-08-21 18:00:02 +0430

    add svg files and try to add station schematic

--- panel/src/station/calculation/CalculationCreate.js
+++ panel/src/station/calculation/CalculationCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {required, SelectInput,ReferenceField, maxLength, Create, Edit, SimpleForm, ReferenceInput, DisabledInput, TextInput, DateInput, LongTextInput, ReferenceManyField, Datagrid, TextField, DateField, EditButton, NumberInput } from  '../../core';
+import Pipe from '../../svgfile/pipe/pipe(1).svg';
+import FourWay from '../../svgfile/fourWay/pipe(2).svg';
 
 const validateCalculationCreation = (values) => {
 
@@ -28,8 +30,31 @@ export const CityGateStationCreate = (props) => (
             <ReferenceInput label="Natural Gas" source="gas.id" reference="gass" validate={required} allowEmpty>
                 <SelectInput source="name" />
             </ReferenceInput>
+            <div key='resultDiv4' className='rowC' style={{ height: 'auto !important', margin: 'auto', width: 500 }}>
+                <div className='rowC'>
+                <div className='rowC'>
+                    <section>
+                        <img src={Pipe}  />
+                    </section>
+                </div>
+                <div className='rowC'>
+                    <section >
+                        <img src={FourWay} className='rowC'  />
+                    </section>
+                </div>
+                </div>
+
+            </div>
+
         </SimpleForm>
     </Create>
 );
+
+const divStyle = {
+    margin: '40px',
+    border: '5px solid pink',
+    display: 'flex',
+    alignItems: 'center'
+};
 
 export default CityGateStationCreate;
