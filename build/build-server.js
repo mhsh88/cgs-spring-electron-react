c2dc66d31c4daeb83507f19c3f11cec88bd9a728
hossein sharifi <hosseinsharifi@hotmail.com>
2018-09-02 12:31:54 +0000

    implement h2 db

--- build/build-server.js
+++ build/build-server.js
@@ -7,17 +7,17 @@ shell.echo('##########################')
 shell.cd('panel')
 const PUBLIC = '../spring/src/main/resources/public/'
 const STATIC = '../spring/src/main/resources/static/'
-const remplates = '../spring/src/main/resources/templates/'
+const templates = '../spring/src/main/resources/templates/'
 shell.rm('-rf', PUBLIC);
 shell.rm('-rf', STATIC);
-shell.rm('-rf', remplates);
+shell.rm('-rf', templates);
 if (shell.exec('npm run build').code !== 0) {
   shell.echo('Error: react build failed')
   shell.exit(1)
 }
 shell.cp('-R', 'build/', PUBLIC)
 shell.cp('-R', 'build/', STATIC)
-shell.cp('-R', 'build/', remplates)
+shell.cp('-R', 'build/', templates)
 shell.cd('..')
 
 shell.echo('##########################')
